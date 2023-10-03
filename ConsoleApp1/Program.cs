478d272d7a1d6ee760b391678a30eb9ed0716dc3
Esiw[Ярослав] <65117234+Esiw12@users.noreply.github.com>
2023-10-03 18:06:49 +0300

    rpg game
    C# Рпг игра  как сбежать с пары.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите третье число: ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите четвертое число: ");
+        int number4 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2 + number3 + number4;
+        double sr = (double)sum / 4;
+
+        Console.WriteLine("Среднее значение: " + sr);
+    }
+}
